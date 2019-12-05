89313ab0a353f183d023fb049e5aa5f6f08614d4
hua.zhou <hua.zhou@clobotics.com>
2019-12-05 21:04:20 +0800

    delete useless messages

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhouhua/ros_new_mav/src/offboard
-# Build directory: /home/zhouhua/ros_new_mav/src/offboard/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
